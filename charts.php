a2e28bf23d23f8fc10197b6ef501d1533880ed08
vbmcz <151908083+vbmcz@users.noreply.github.com>
2023-11-24 12:36:37 +0100

    Update charts.php
    Added working graphs,
    TODO: query to get data(values)

--- charts.php
+++ charts.php
@@ -1,21 +1,31 @@
 <?php
-require_once "config.php"; // Tutaj załóżmy, że masz plik konfiguracyjny do połączenia z bazą danych
+require_once "config.php";
 
-// Zapytanie do pobrania nazw kategorii z bazy danych
-$query = "SELECT category_name FROM categories";
-$result = mysqli_query($link, $query); // Zakładam, że używasz MySQLi
+$query = "SELECT content FROM categories";
+$result = $conn->query($query);
 
 $categories = array();
 
-while ($row = mysqli_fetch_assoc($result)) {
-    $categories[] = $row['category_name'];
+while ($row = $result->fetch_assoc()) {
+    $categories[] = $row['content'];
 }
+// TODO:
+// $query = "SELECT amount FROM expenses WHERE expenses.id = categories.expense_id "
 
-mysqli_close($link);
+// $query = "INSERT INTO categories(expense_id, content) VALUES(1, 'umiejętności artura')";
+// $query1 = "INSERT INTO categories(expense_id, content) VALUES(1, 'umiejętności ar1tura')";
+// $query2= "INSERT INTO categories(expense_id, content) VALUES(1, 'umiejętno1ści artura')";
+// $query3 = "INSERT INTO categories(expense_id, content) VALUES(1, 'umiejętności ar3tura')";
+
+// $conn->query($query);
+// $conn->query($query1);
+// $conn->query($query2);
+// $conn->query($query3);
+
+$conn->close();
 ?>
 
 
-?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -51,48 +61,89 @@ mysqli_close($link);
 </body>
 
 <script>
+    
+    const colors = ["red", "blue", "green", "yellow", "cyan", "gold"];
 
     
-  var categories = <?php echo json_encode($categories); ?>;
+    var categories = <?php echo json_encode($categories); ?>;
+
+    var datasets = [];
+
+    
+    for (var i = 0; i < categories.length; i++) {
+        var dataset = {
+            data: [10,20,30,40],
+            label: categories[i], 
+            borderColor: colors[i], 
+            fill: false
+        };
+        datasets.push(dataset);
+    }
 
     new Chart(document.getElementById("myChart"), {
-    type: 'line',
-    data: {
-        labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050, 1999, 2050,],
-        datasets: [{
-            data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478, 783, 2478],
-            label: categories,
-            borderColor: "#3e95cd",
-            fill: false
-        }, {
-            data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
-            label: categories,
-            borderColor: "#8e5ea2",
-            fill: false
-        }, {
-            data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
-            label: categories,
-            borderColor: "#3cba9f",
-            fill: false
-        }, {
-            data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
-            label: categories,
-            borderColor: "#e8c3b9",
-            fill: false
-        }, {
-            data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
-            label: categories,
-            borderColor: "#c45850",
-            fill: false
+        type: 'line',
+        data: {
+            labels: categories,
+            datasets: datasets 
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Wydatki'
+            }
         }
-        ]
-    },
-    options: {
-        title: {
-            display: true,
-            text: '',
-        }
-    }
-});
+    });
+
+
+    // var categories = <?php// echo json_encode($categories); ?>;
+    // var datasets = [];
+    // for(var i = 0; i < categories.length; ++i)
+    // {
+    //     var dataset = [
+    //         {
+    //             data: [10],
+    //             label: categories[i],
+    //             borderColor: "#3e95cd",
+    //             fill: false
+    //         }
+    //     ]
+    //     datasets.push(dataset);
+    // }
+    
+    // new Chart(document.getElementById("myChart"), {
+    // type: 'line',
+    // data: {
+    //     labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050, 1999, 2050,],
+    //     datasets: datasets,
+        // {
+        //     data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+        //     label: categories[1],
+        //     borderColor: "#8e5ea2",
+        //     fill: false
+        // }, {
+        //     data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+        //     label: categories,
+        //     borderColor: "#3cba9f",
+        //     fill: false
+        // }, {
+        //     data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+        //     label: categories,
+        //     borderColor: "#e8c3b9",
+        //     fill: false
+        // }, {
+        //     data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+        //     label: categories,
+        //     borderColor: "#c45850",
+        //     fill: false
+        // }
+        // ]
+//     },
+//     options: {
+//         title: {
+//             display: true,
+//             text: '',
+//         }
+//     }
+// });
 </script>
 </html>
