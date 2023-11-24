8e73f4493edbcc2bde989f26ca3daf81b9f9d675
vbmcz <151908083+vbmcz@users.noreply.github.com>
2023-11-24 11:16:01 +0100

    Updated look

--- loginUser.php
+++ loginUser.php
@@ -1,17 +1,17 @@
 <?php
-require_once "config.php";
-session_start();
-$email = $_POST['email'];
-$password = $_POST['password'];
-$query = "SELECT * FROM user WHERE email ='$email' AND password = '$password' LIMIT 1;";
+    require_once "config.php";
+    session_start();
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $query = "SELECT * FROM user WHERE email ='$email' AND password = '$password' LIMIT 1;";
 
-$res = $conn->query($query) or die($conn->errno);
+    $res = $conn->query($query) or die($conn->errno);
 
-$row = $res->fetch_assoc();
+    $row = $res->fetch_assoc();
 
-$_SESSION['id'] = $row['id'];
-$_SESSION['name'] = $row['name'];
+    $_SESSION['id'] = $row['id'];
+    $_SESSION['name'] = $row['name'];
 
-header("location: index.php");
+    header("location: index.php");
 
 ?>
--- registerUser.php
+++ registerUser.php
@@ -1,28 +1,30 @@
 <?php
 
-require_once "config.php";
-$name = $_POST['name'];
-$email = $_POST['email'];
-$password = $_POST['password1'];
+    require_once "config.php";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password1'];
 
-$query = "SELECT * FROM user WHERE email = '$email';";
+    $query = "SELECT * FROM user WHERE email = '$email';";
 
-$res = $conn->query($query) or die($conn->errno);
-if($res->num_rows > 0)
-{
-    echo "Adres email jest już zajęty. Czy chcesz się zalogować?";
-    echo "<a href='login.html'>Zaloguj się</a>";
-    echo "<a href='register.html'>Spróbuj ponownie</a>";
-    exit();
-}
+    $res = $conn->query($query) or die($conn->errno);
+    if($res->num_rows > 0)
+    {
+        echo "Adres email jest już zajęty. Czy chcesz się zalogować?";
+        echo "<a href='login.html'>Zaloguj się</a>";
+        echo "<a href='register.html'>Spróbuj ponownie</a>";
+        exit();
+    }
 
-$query = "INSERT INTO user(name, email, password) VALUES('$name', '$email', '$password')";
+    $query = "INSERT INTO user(name, email, password) VALUES('$name', '$email', '$password')";
 
-if($conn->query($query))
-{
-    header("location: login.html");
-}else{
-    die($conn_>errno);
-}
+    if($conn->query($query))
+    {
+        header("location: login.html");
+    }
+    else
+    {
+        die($conn_>errno);
+    }
 
 ?>
