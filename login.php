57005877667f63253f819f6ae4608435f77dd423
Yo FIlip <filipxpp@wp.pl>
2023-12-01 01:10:10 +0100

    Update #1
    Added login system and tables system for users

--- login.php
+++ login.php
@@ -1,28 +1,44 @@
 <?php
-    session_start();
+session_start();
 
-    if ((isset($_SESSION['logged_in'])) && ($_SESSION['logged_in'] == true)) {
-        header('Location: tabela.php');
-        exit();
+if ((!isset($_POST['email'])) || (!isset($_POST['password']))) {
+    header('Location: login_page.php');
+    exit();
+}
+
+require_once "connect.php";
+$connection = @new mysqli($host, $db_user, $db_password, $db_name);
+
+if ($connection->connect_errno != 0) {
+    echo "Error: " . $connection->connect_errno;
+} else {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $email = htmlentities($email, ENT_QUOTES, "UTF-8");
+
+    if ($result = @$connection->query(sprintf("SELECT * FROM user WHERE email='%s'", mysqli_real_escape_string($connection, $email)))) {
+        $num_users = $result->num_rows;
+        if ($num_users > 0) {
+            $row = $result->fetch_assoc();
+            if (password_verify($password, $row['password'])) {
+                $_SESSION['logged_in'] = true;
+                $_SESSION['id'] = $row['id'];
+                $_SESSION['user'] = $row['username'];
+
+                unset($_SESSION['error']);
+                $result->free_result();
+                header('Location: tabela.php');
+            } else {
+                $_SESSION['error'] = '<span style="color:red">Invalid email or password!</span>';
+                header('Location: login_page.php');
+            }
+        } else {
+            $_SESSION['error'] = '<span style="color:red">Invalid email or password!</span>';
+            header('Location: login_page.php');
+        }
     }
+
+    $connection->close();
+}
 ?>
-
-<!DOCTYPE HTML>
-<html lang="pl">
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Login Page</title>
-</head>
-
-<body>
-    <form action="zaloguj.php" method="post">
-        Username: <br /> <input type="text" name="username" /> <br />
-        Password: <br /> <input type="password" name="password" /> <br /><br />
-        <input type="submit" value="Log in" />
-    </form>
-
-    <?php if (isset($_SESSION['error'])) echo $_SESSION['error']; ?>
-</body>
-
-</html>
--- tabela.php
+++ tabela.php
@@ -1,20 +1,86 @@
 <?php
-    session_start();
-    
-    if (!isset($_SESSION['logged_in'])) {
-        header('Location: login.php');
-        exit();
-    }
+session_start();
+require_once "connect.php";
+
+if (!isset($_SESSION['logged_in'])) {
+    header('Location: login_page.php');
+    exit();
+}
+
+$connection = new mysqli($host, $db_user, $db_password, $db_name);
+
+if ($connection->connect_errno != 0) {
+    echo "Error: " . $connection->connect_errno;
+    exit();
+}
+
+$user_id = $_SESSION['id'];
+
+// Obliczanie sumy wydatków
+$total_expenses = 0;
+$sql = "SELECT SUM(amount) AS total FROM expenses WHERE user_id='$user_id' AND amount IS NOT NULL";
+$result = $connection->query($sql);
+if ($result && $row = $result->fetch_assoc()) {
+    $total_expenses = $row['total'];
+}
+
+// Obliczanie sumy dochodów
+$total_income = 0;
+$sql = "SELECT income FROM user WHERE id='$user_id'";
+$result = $connection->query($sql);
+if ($result && $row = $result->fetch_assoc()) {
+    $total_income = $row['income'];
+}
+
+// Obliczanie podsumowania budżetu
+$budget_summary = $total_income - $total_expenses;
+
 ?>
 
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
     <meta charset="utf-8" />
-    <title>Table</title>
+    <title>Tabela Wydatków</title>
 </head>
 <body>
-    <h1>Data Table</h1>
-    <a href="logout.php">Log out</a>
+    <h1>Tabela Wydatków</h1>
+    <a href="logout.php">Wyloguj</a>
+
+    <h2>Podsumowanie Budżetu</h2>
+    <p>Całkowite zarobki: <?php echo $total_income; ?><br>
+       Całkowite wydatki: <?php echo $total_expenses; ?><br>
+       Stan budżetu: <?php echo $budget_summary; ?></p>
+
+    <h2>Dodaj Dochód</h2>
+    <form action="add_expense.php" method="post">
+        Kwota Dochodu: <br /> <input type="number" name="profit" step="0.01" required /> <br />
+        <input type="submit" name="submit_income" value="Dodaj dochód" />
+    </form>
+
+    <h2>Dodaj Wydatek</h2>
+    <form action="add_expense.php" method="post">
+        Data: <br /> <input type="date" name="date" required /> <br />
+        Opis: <br /> <input type="text" name="description" required /> <br />
+        Kwota Wydatku: <br /> <input type="number" name="amount" step="0.01" required /> <br />
+        <input type="submit" name="submit_expense" value="Dodaj wydatek" />
+    </form>
+
+    <h2>Wydatki</h2>
+    <?php
+    $sql = "SELECT date, description, amount FROM expenses WHERE user_id='$user_id' AND amount IS NOT NULL ORDER BY date DESC";
+    $result = $connection->query($sql);
+
+    if ($result && $result->num_rows > 0) {
+        echo "<table border='1'><tr><th>Data</th><th>Opis</th><th>Kwota</th></tr>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr><td>" . $row["date"] . "</td><td>" . $row["description"] . "</td><td>" . $row["amount"] . "</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "Brak wydatków.";
+    }
+    $connection->close();
+    ?>
 </body>
 </html>
