6080d133994980a542f016cf5a174221c4bf996e
vbmcz <151908083+vbmcz@users.noreply.github.com>
2024-01-05 08:45:35 +0100

    Update create_file.php
    Created a working pdf file generator

--- php/create_file.php
+++ php/create_file.php
@@ -12,31 +12,80 @@ $conn = new mysqli($host, $db_user, $db_password, $db_name) or die("Błąd: ".$c
 
 $user_id = $_SESSION['id'];
 
-$query = "SELECT expenses.*, SUM(expenses.amount) AS sum categories.title AS category_title 
-FROM expenses 
-JOIN categories ON expenses.expense_id = categories.categorie_id 
-WHERE expenses.user_id = '$user_id'";
+$query = "SELECT SUM(expenses.amount) AS sum FROM expenses WHERE expenses.user_id = '$user_id'";
 
-// $res = $conn->query($query);
+$res = $conn->query($query);
 
-// $sum = 0;
+$sum = 0;
 
-// if($res)
-// {
-//     while($row = $res->fetch_assoc())
-//     {
-//         $sum = $row['sum'];
-//     }
-// }
+if($res)
+{
+    while($row = $res->fetch_assoc())
+    {
+        $sum = $row['sum'];
+    }
+}
+
+$res = $conn->query(
+    $query = "SELECT expenses.*, categories.title AS category_title 
+    FROM expenses 
+    JOIN categories ON expenses.expense_id = categories.categorie_id 
+    WHERE expenses.user_id = '$user_id'
+    ORDER BY date");
 
 $pdf->setFont('Arial', 'B', 30);
 
 $pdf->Cell(0, 10, utf8_decode("User raport"), 0 , 1, "C");
 $pdf->Ln();
+$pdf->Ln();
 
 $pdf->setFont('Arial', '', 15);
-// $pdf->Cell(0, 10, "Wydatki: $sum");
+$pdf->Cell(0, 10, "Expenses: ".$sum."pln");
+$pdf->Ln();
+$pdf->Ln();
 
+$pdf->setFont('Arial', 'B', 20);
+$pdf->Cell(0, 10, "Your expenses: ", 0, 1, "C");
+$pdf->Ln();
+
+$pdf->setFont('Arial', '', 15);
+
+// Setting universal column width/length
+$col_length = 47;
+
+// Setting universal column height
+$col_height = 10;
+
+$pdf->Cell($col_length, $col_height, "Description", 1, 0, 'C');
+$pdf->Cell($col_length, $col_height, "Amount", 1, 0, 'C');
+$pdf->Cell($col_length, $col_height, "Categorie", 1, 0, 'C');
+$pdf->Cell($col_length, $col_height, "Date", 1, 1, 'C');
+
+if($res)
+{
+    foreach($res as $row)
+    {
+        $pdf->Cell($col_length, $col_height, $row['description'], 1, 0, 'C');
+        $pdf->Cell($col_length, $col_height, $row['amount'], 1, 0, 'C');
+        $pdf->Cell($col_length, $col_height, $row['category_title'], 1, 0, 'C');
+        $pdf->Cell($col_length, $col_height, $row['date'], 1, 1, 'C');
+    }
+}
+
+$pdf->Ln();
+$pdf->Ln();
+$pdf->Ln();
+
+$res = $conn->query("SELECT income FROM user WHERE id = '$user_id'");
+
+if ($res) {
+    $temp = $res->fetch_assoc();
+    $income = $temp['income'];
+    $pdf->Cell(0, 10, "Income: ".$income."pln");
+}
+else{
+    $pdf->Cell(0, 10, "Income: None");
+}
 $pdf->Output();
 ?>
 								
