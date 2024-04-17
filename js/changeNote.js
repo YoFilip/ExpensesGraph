18def6f2b5ee4eb905437d0a51513666ecc933a3
Yo FIlip <102390337+YoFilip@users.noreply.github.com>
2024-04-17 21:00:30 +0200

    Add files via upload

---
+++ js/changeNote.js
@@ -0,0 +1,24 @@
+function changeNote(id) {
+    let note_id = id - 1;
+
+    let text = document.getElementsByClassName('content')[note_id].innerHTML;
+    let title = document.getElementsByClassName('title')[note_id].innerHTML;
+    const note = document.getElementsByClassName('note')[note_id];
+
+    let form = "<form id='changeForm' action='../php/update_note.php' method='post'>";
+
+    let out = form.concat("<label style='margin-bottom: 0;'>Edit Title: </label><br>",
+        `<input type='text' value='${title}' name='title'>`,
+        `<label>Edit Description: </label> <br> <textarea rows='3' cols='30' name='content'>${text}</textarea><br>`,
+        `<button class='notesBtn'type='submit' name='btnId' value='${id}'>Edit Note</button>`,
+
+        "</form>",
+    );
+
+
+
+    note.innerHTML = out;
+
+    note.innerHTML += `<form id='formNone' action='../php/delete_note.php' method='post'><button class='delateBtn' name='btn' value='${id}' type='submit'>Delete Note</button></form>`;
+
+}
---
+++ js/exchangerates.js
@@ -0,0 +1,47 @@
+function updateExchangeRates() {
+    var apiKey = 'Your_api_kay';
+    var fromCurrency = document.getElementById('from-currency').value;
+    var exchangeRatesUrl = 'https://v6.exchangerate-api.com/v6/' + apiKey + '/latest/' + fromCurrency;
+
+    fetch(exchangeRatesUrl)
+        .then(response => response.json())
+        .then(data => {
+            var rates = data.conversion_rates;
+            var tableBody = document.getElementById('exchange-rates-body');
+            tableBody.innerHTML = '';
+
+            for (var currency in rates) {
+
+                var row = tableBody.insertRow();
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                cell1.innerHTML = currency;
+                cell2.innerHTML = rates[currency];
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching exchange rates:', error);
+        });
+}
+
+setInterval(updateExchangeRates, 300000000000);
+
+function calculateCurrency() {
+    var amount = document.getElementById('amount').value;
+    var fromCurrency = document.getElementById('from-currency').value;
+    var toCurrency = document.getElementById('to-currency').value;
+    var apiKey = 'Your_api_kay';
+    var apiUrl = 'https://v6.exchangerate-api.com/v6/' + apiKey + '/latest/' + fromCurrency;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            var exchangeRate = data.conversion_rates[toCurrency];
+            var result = amount * exchangeRate;
+            document.getElementById('result').innerHTML = 'Converted currency: ' + result.toFixed(2) + ' ' + toCurrency;
+        })
+        .catch(error => {
+            console.error('Error fetching exchange rates:', error);
+        });
+    updateExchangeRates();
+};
